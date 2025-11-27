66b7cce90cea690c915d9f1701703c20165de701
Aviraj <avirajkarad@gmail.com>
2025-11-27 15:47:10 +0530

    Added roles to users

---
+++ Models/Roles.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace eOrderTouchApp.Models
+{
+    public class Roles
+    {
+        public string key { get; set; }
+        public string value { get; set; }
+
+        public static List<Roles> GetRoles()
+        {
+            List<Roles> rl = new List<Roles>();
+            rl.Add(new Roles() { value="Owner", key= "Owner" });
+            rl.Add(new Roles() { value = "User", key = "User" });
+
+            return rl;
+        }
+
+    }
+}
