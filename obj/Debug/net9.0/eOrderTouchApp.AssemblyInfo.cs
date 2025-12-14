33d61ebfeba12e6b8f833cabfd41394fba50e3a2
Aviraj <avirajkarad@gmail.com>
2025-12-14 23:46:28 +0530

    Merge with Report

--- obj/Debug/net9.0/eOrderTouchApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/eOrderTouchApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("eOrderTouchApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23214075338a80093b2581cb1926b28818a0ab96")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd1c11818bfd2ed1b293ba6e56ae1041fd1d1af8")]
 [assembly: System.Reflection.AssemblyProductAttribute("eOrderTouchApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("eOrderTouchApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
