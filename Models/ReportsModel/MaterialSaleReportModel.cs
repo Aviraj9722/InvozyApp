c0764023313de0716af8d277f70ff4a29a0d3620
Pranit <Pranit@DESKTOP-GQDD1DT>
2025-12-16 12:22:36 +0530

    models

---
+++ Models/ReportsModel/MaterialSaleReportModel.cs
@@ -0,0 +1,13 @@
+﻿namespace eOrderTouchApp.Models.ReportsModel
+{
+    public class MaterialSaleReportModel
+    {
+        public string? MaterialName { get; set; }
+        public string? Category { get; set; }
+        public string? HSNCode { get; set; }
+        public string? Barcode { get; set; }
+
+        public decimal? TotalQty { get; set; }
+        public decimal? TotalSale { get; set; }
+    }
+}
